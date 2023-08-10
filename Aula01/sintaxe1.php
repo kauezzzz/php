a57d37c1dd91a09565b7c6660bbc2ce438afe867
kauezzzz <kauedosenai@gmail.com>
2023-08-10 14:29:47 -0300

    up all sintaxe

---
+++ Aula01/sintaxe1.php
@@ -0,0 +1,8 @@
+<?php
+    // Geração de texto (string)
+    echo "Chama Sesi-Senai !";
+
+    /* Geração de texto estruturação (com tags, atributos) */
+    echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP.</h2>";
+    echo "<h2>Gerando <span class=\"destaque\">HTML</span> através do PHP.</h2>";
+?>
---
+++ Aula01/sintaxe3.php
@@ -0,0 +1,12 @@
+<?php   
+echo                                   "testando espaço em branco <br>";
+echo "testando espaço <br>"                ;
+
+//O caractere &nbsp; adiciona um espaço em branco dentro da string
+echo "&#160;   &#160;                                testando       espaço       em        branco <br>";
+"quebra de linha <br>"
+;
+
+echo "quebra de linha <br>"
+;
+?>
