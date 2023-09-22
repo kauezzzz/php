6e43a495b3d15b70b2cdafc3b0936a6f50e906de
kauezzzz <kauedosenai@gmail.com>
2023-09-22 15:48:48 -0300

    up

--- Aula04_alunos/1_inclusão/04-Inclusao.php
+++ Aula04_alunos/1_inclusão/04-Inclusao.php
@@ -1,6 +1,6 @@
 <!-- Fazendo a inclusão de um arquivo de programação -->
 
-<!-- 1ª Digitação (Aqui) -->
+<?php include "recursos.php"; ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -20,11 +20,11 @@
 
         <!-- Recuperando o conteúdo da string Escola (que é Senai) definido em "recursos.php" -->
 
-        <!-- 2ª Digitação (Aqui) -->
+        <h2 class="destaque"><?=ESCOLA?></h2>
 
         <!-- Recuperando o conteúdo da variável $curso definida em "recursos.php" -->
 
-        <!-- 3ª Digitação (Aqui) -->
+        <p>estamos fazendo o curso de <?=curso?></p>
 
         <!-- Recuperando o conteúdo do array $tecnologia definido em "recursos.php" e criando uma lista com foreach -->
         <ul>
@@ -37,7 +37,7 @@
 
         <!-- Chamando a função VerificaIdade criara em "recursos.php" para retornar se é maior ou menor -->
         
-        <!-- 4ª Digitação (Aqui) -->
+        <p>o aluno Chapolin Colorado tem 25 anos e é: <?=verificaIdade(25)?> de idade</p>
 
         <hr>
 
@@ -46,7 +46,7 @@
             <h2 class="destaque">Conteúdo adicional</h2>
             <!-- No caso de erros o required para tudo e o include não -->
             
-            <!-- 5ª Digitação (Aqui) -->
+            <?php include "Textos.php"?>
 
         </article>
         
